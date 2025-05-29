792f0c3decec3e879c84c1e65080ab6b031a473f
CarterPlenge <cplenge22@gmail.com>
2025-05-28 20:22:37 -0500

    update pt2

--- Syntax/pt2-cout.cpp
+++ Syntax/pt2-cout.cpp
@@ -14,5 +14,9 @@ int main() {
     cout << "Hello" << " World!" << endl;
     cout << "Hello" << " " << "World!" << endl;
 
+    // we can also do math similar to pythons print function
+    cout << "2 + 2 = " << 2 + 2 << endl;
+
+    // you can also use "\n" to insert a newline
     return 0;
 }
