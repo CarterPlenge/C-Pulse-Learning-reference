9aa36595607f54eac01a73a2a680e9c8be92b97e
CarterPlenge <cplenge22@gmail.com>
2025-05-28 22:36:57 -0500

    pt3 updated

---
+++ Syntax/pt3-input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // we get input from the user using cin
+    // its syntax is similar to cout
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    
+}
