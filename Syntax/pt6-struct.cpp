b0f99bd01bce693b8d99a49fda9064f47ead6d28
CarterPlenge <cplenge22@gmail.com>
2025-05-28 22:37:48 -0500

    pt6 init

---
+++ Syntax/pt6-struct.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+struct character {
+    string name;
+    int age;
+    string role;
+    double health;
+    bool isHero;
+
+    // You can also define member functions inside a struct
+    void attack() {
+        cout << name << " attacks!" << endl;
+    }  
+}
+
+main() {
+    character Alice;
+    Alice.name = "Alice";
+    Alice.age = 25;
+    Alice.role = "Wizard";
+    Alice.health = 50.0;
+    Alice.isHero = true;
+
+    cout << "Character Name: " << Alice.name << endl;
+
+    character Bob {"Bob", 30, "Warrior", 100.0, false};
+    cout << "Character Name: " << Bob.name << endl;
+}
