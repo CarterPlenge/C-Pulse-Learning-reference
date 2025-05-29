a4b1a950573563e27da2228cc0c74a2b5a655e2d
CarterPlenge <cplenge22@gmail.com>
2025-05-28 20:22:29 -0500

    update pt1

--- Syntax/pt1.cpp
+++ Syntax/pt1.cpp
@@ -16,6 +16,24 @@ int main() {
     // ITS C-OUT NOT COUNT, stop miss reading it
     cout << "Hello World!";
 
+    // variable follow the follwing format
+    // type variable_name = value;
+    // for example:
+    int num = 5;
+    double pi = 3.14;
+    char myLetter = 'A';
+    string name = "Alice";
+    bool isTrue = true;
+
+    // simmilar to python we can declare multiple variables at once
+    int a = 1, b = 2, c = 3;
+
+    // const is a thing if you want it
+    const double piValue = 3.14159;
+    const int minutesPerHour = 60;
+
+    
+
     // ends main and returns 0 to the os, indicating that the program ran successfully
     return 0;
 }
